be4c247849cd6c573bc388ee45fd9fd1b066aed4
Vikas Mishra <vikasmishra.webdev@gmail.com>
2025-01-10 23:24:00 +0530

    fixing auth issue

--- controller/Auth.controller.js
+++ controller/Auth.controller.js
@@ -2,7 +2,7 @@ import { User } from "../model/User.model.js";
 
 export const createUser = async (req, res) => {
   try {
-    const user = await User.create(req.body).exec();
+    const user = await User.create(req.body);
     res.status(200).json(user);
   } catch (error) {
     res.status(400).json({ message: error.message });
@@ -15,14 +15,12 @@ export const loginUser = async (req, res) => {
     if (!user) {
       res.status(401).json({ message: "invalid credentials" });
     } else if (user.password === req.body.password) {
-      res
-        .status(200)
-        .json({
-          id: user.id,
-          name: user.name,
-          email: user.email,
-          role: user.role,
-        });
+      res.status(200).json({
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      });
     } else {
       res.status(401).json({ message: "invalid credentials" });
     }
